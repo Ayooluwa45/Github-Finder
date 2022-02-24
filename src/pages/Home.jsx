b2c7a5e496670ddc639258921ee84b0d78dad066
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-02-24 15:56:05 +0100

    Created a new Spinner components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserResult from "../Components/users/UserResult";
+
+function Home() {
+  return (
+    <>
+      <UserResult />
+    </>
+  );
+}
+
+export default Home;
