1836fce48f0632b5de22fb7096e2cf851b76c5fb
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-02-27 06:42:17 +0100

    User Component created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import UserResult from "../Components/users/UserResult";
+import UserResults from "../Components/users/UserResults";
+
 
 function Home() {
   return (
     <>
-      <UserResult />
+     <UserResults/>
     </>
   );
 }
