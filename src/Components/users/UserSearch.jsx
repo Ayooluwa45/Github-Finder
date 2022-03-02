54a70c8875e1a4fbd92005d5042f7bf4ec8ac7d5
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-03-02 22:10:03 +0100

    gitignore added

--- src/Components/users/UserSearch.jsx
+++ src/Components/users/UserSearch.jsx
@@ -1,24 +1,23 @@
-import {useState, useContext} from "react";
-import GithubContext from "../../context/github/GithubContext"
-
+import { useState, useContext } from "react";
+import GithubContext from "../../context/github/GithubContext";
 
 function UserSearch() {
-  const {users, searchUsers, clearUsers} = useContext(GithubContext);
-  const [text, setText]= useState('')
+  const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const [text, setText] = useState("");
 
-const handleChange= (e)=>{
-setText(e.target.value)
-}
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
 
-const handleSubmit= (e) =>{
-  e.preventDefault()
-  if(text === ''){
-    alert('Please enter something')
-  }else{
-    searchUsers(text)
-    setText('')
-  }
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text === "") {
+      alert("Please enter something");
+    } else {
+      searchUsers(text);
+      setText("");
+    }
+  };
 
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
@@ -40,14 +39,17 @@ const handleSubmit= (e) =>{
           </div>
         </form>
       </div>
-      {users.lenght>0 &&(
+      {users.length > 0 && (
         <div>
-        <button onClick={clearUsers} type="submit" className="btn btn-ghost btn-lg">
-          clear
-        </button>
-      </div>
+          <button
+            onClick={clearUsers}
+            type="submit"
+            className="btn btn-ghost btn-lg"
+          >
+            clear
+          </button>
+        </div>
       )}
-     
     </div>
   );
 }
