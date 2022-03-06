caa4a0b367700f00ad2850d463ba0590c1524426
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-03-06 20:05:27 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/layout/Navbar";
 import Footer from "./Components/layout/Footer";
+import Alert from "./Components/users/Alert";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import User from "./pages/User";
 import {GithubProvider} from "./context/github/GithubContext"; 
+import {AlertProvider} from "./context/alert/AlertContext"; 
 
 function App() {
   return (
      <GithubProvider> 
+     <AlertProvider> 
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
+        <Alert/>
         <main className="container mx-auto px-3 pb-12">
           <Routes>
             <Route path='/' element={<Home/>} />
             <Route path='/about' element={<About/>} />
+            <Route path='/user/:login' element={<User/>} />
             <Route path='/notFound' element={<NotFound/>} />
             <Route path='/*' element={<NotFound/>} />
           </Routes>
@@ -23,6 +29,7 @@ function App() {
         <Footer />
       </div>
     </Router>
+    </AlertProvider> 
     </GithubProvider> 
   );
 }
