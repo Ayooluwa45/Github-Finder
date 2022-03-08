ca804a7abade068e3a6538b9050c34915573a3ed
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-03-08 10:00:55 +0100

    New Updates

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,7 +5,7 @@ import Spinner from "../Components/layout/Spinner";
  import RepoList from "../Components/repos/RepoList"; 
 import { useParams } from "react-router-dom";
 import GithubContext from "../context/github/GithubContext";
-import { getUser, getUserRepos} from "../context/github/GithubAction";
+import { getUserAndRepos} from "../context/github/GithubAction";
 
 
 function User() {
@@ -16,12 +16,9 @@ function User() {
     useEffect(() => {
       dispatch({type: 'SET_LOADING'})
   const getUserData = async()=>{
-    const userData= await getUser(params.login)
-    dispatch({type: 'GET_USER', payload: userData})
+    const userData= await getUserAndRepos(params.login)
+    dispatch({type: 'GET_USER_AND_REPOS', payload: userData})
   
-
-  const userRepoData= await getUserRepos(params.login)
-  dispatch({type: 'GET_REPOS', payload: userRepoData})
   }
   getUserData()
      /*  //eslint-disable-next-line react-hooks/exhaustive-deps */
